9f932f477e4bbd1edcd55160a7ff053d7f260efb
JohanC25 <johanmcm@hotmail.com>
2023-10-26 16:26:02 -0500

    Subir a github

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MultasController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::resource('prestamos', App\Http\Controllers\PrestamoController::class)->middleware('auth');
+Route::resource('equipos', App\Http\Controllers\EquipoController::class)->middleware('auth');
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/multas', [MultasController::class, 'index'])->name('multas.index'); //cuando utilizo get solicito un recurso de la vista index
+Route::post('/multas', [MultasController::class, 'filtrar'])->name('multas.filtrar'); //cuando hago post para hacer un envio de datos con la función filtrar
+
